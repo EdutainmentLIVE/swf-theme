d03b47a6c813adb18022ec70ee5789b0bc508f4f
Kiyota <kiyota@swfsoft.com>
2020-04-30 15:57:20 -0700

    Clean up and update readme

--- src/index copy.js
+++
@@ -1,4 +0,0 @@
-export { default } from './theme'
-export * from './theme'
-export { default as Palette } from './components/Palette'
-export { default as FullPalette } from './components/FullPalette'
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-export { default } from './theme'
 export * from './theme'
 export { default as Palette } from './components/Palette'
 export { default as FullPalette } from './components/FullPalette'
